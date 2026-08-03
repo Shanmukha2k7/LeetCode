a46318cdc14a8d64aab5715cc598ca04c04dd985
Kaitepalli Shanmukha Vardhan <shanmukha2k7@gmail.com>
2026-08-03 14:15:03 +0530

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- hash-table/two-sum.java
+++ hash-table/two-sum.java
@@ -1,11 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] a=new int[2];
-        for(int i=1;i<nums.length;i++){
-            if(nums[i-1]+nums[i]==target){
-                a[0]=i-1;
-                a[1]=i;
-                break;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;i++){
+                if(nums[i]+nums[j]==target){
+                    a[0]=i;
+                    a[1]=j;
+                    break;
+                }
             }
         }
         return a;
